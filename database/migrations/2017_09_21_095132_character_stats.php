15cfc3cafcec9d094103269543f259dc39325a61
Mariusz Patyk <patyk.mariusz@gmail.com>
2017-09-21 10:33:22 +0000

    created character stats table migration

---
+++ database/migrations/2017_09_21_095132_character_stats.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CharacterStats extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('character_stats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('character_id')
+            ->unique();
+            $table->foreign('character_id')
+            ->references('id')->on('characters')
+            ->onDelete('cascade');
+            $table->unsignedSmallInteger('strength');
+            $table->unsignedSmallInteger('stamina');
+            $table->unsignedSmallInteger('quickness');
+            $table->unsignedSmallInteger('dexterity');
+            $table->unsignedSmallInteger('intelligence');
+            $table->unsignedSmallInteger('willpower');
+            $table->unsignedSmallInteger('perceptiveness');
+            $table->unsignedSmallInteger('acumen');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('character_stats');
+    }
+}
